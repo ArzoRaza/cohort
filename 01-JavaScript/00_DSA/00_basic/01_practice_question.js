22db21a5d99c1adc92e41522be78ceccd4046c3e
ArzoRaza <ccoding76@gmail.com>
2025-03-21 23:50:18 +0530

    js update

---
+++ 01-JavaScript/00_DSA/00_basic/01_practice_question.js
@@ -0,0 +1,20 @@
+// Q1: Calculate area and parameter of rectangle
+// let length = 0;
+// let width = 0;
+
+function result(length, width) {
+    const area = length * width;
+    const parameter = 2 * (length + width);
+
+    return `Area is: ${area} and parameter is: ${parameter}`
+
+}
+
+const answer = result(12, 7);
+console.log(answer);
+
+// Q2 Generate OTP. 
+
+// function GenerateOTP() {
+//     console.log(Math.floot(Math.random() * 100))
+// }
---
+++ 01-JavaScript/01_basicc_constructs/script.js
@@ -0,0 +1,7 @@
+let arr = new Array("Apple", "mango", "banana");
+let arr1 = ["Apple", "mango", "banana"];
+
+console.log(arr[0]);
+
+// arr.                //need experiment 
+
