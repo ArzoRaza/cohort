f1e11dec766e6437b694a8c41ccec4415b92e1f9
ArzoRaza <ccoding76@gmail.com>
2025-03-24 14:26:49 +0530

    initialization of first full stack project

---
+++ 03-full-stack-project/00_first_project/README.md
@@ -0,0 +1,6 @@
+1. npm init  // for project initialization
+2. npm i express // for express init to connect bk with f
+3. npm i -D nodemon // for autoupdates 
+4. npm i dotenv // for .env, its main file of project then need to install package so do not need to import directelly. 
+5. npm i cors // for frondend and backend management. 
+6. npm i mongoose
--- 03-full-stack-project/00_first_project/index.js
+++ 03-full-stack-project/00_first_project/index.js
@@ -1,13 +1,36 @@
 import express from "express";
-const app = express()
-const port = 3000
+import dotenv from "dotenv";
+import cors from "cors";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+dotenv.config();
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+const app = express();
 
-console.log(send);
+app.use(
+  cors({
+    origin: "https://localhost:3000", // only allow this frondend
+    methods: ["GET", "POST", "PUT", "DELETE"], // these request allowed
+    allowedHeaders: ["Content-Type", "Authorization", "X-Custom-Header"], //only these thing allow
+  })
+);
+
+app.use(express.json());  // eccept json data
+app.use(express.urlencoded({ extended: true })); // url encoding
+
+const port = process.env.PORT || 4000; // port allocated by server or running on 4000 PORT
+
+app.get("/", (req, res) => {
+  res.send("First Cohort!");
+});
+
+app.get("/arzo", (req, res) => {
+  res.send("Hello, this page dedicated for Arzo");
+});
+
+app.get("/akbar", function (r, s) {
+  s.send("Now it's page dedicated for Akbar");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listening/Running on port ${port}`);
+});
